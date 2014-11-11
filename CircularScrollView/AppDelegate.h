eec49a83b586fd96d626c1247805f6abfff84804
NSSimpleApps <ns.poluyanov.sergey@gmail.com>
2014-11-11 22:48:23 +0600

    Initial Commit

---
+++ CircularScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CircularScrollView
+//
+//  Created by neo on 11.11.14.
+//  Copyright (c) 2014 NSSimpleApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CircularScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CircularScrollView
+//
+//  Created by neo on 11.11.14.
+//  Copyright (c) 2014 NSSimpleApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
